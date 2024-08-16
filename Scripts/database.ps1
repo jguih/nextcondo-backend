73b35b061e657a6093f3c14745df10c6d9a663a3
jguih <gui.kcoal+github@hotmail.com>
2024-08-16 10:42:43 -0300

    Update script folder

---
+++ Scripts/database.ps1
@@ -0,0 +1,55 @@
+
+param(
+  [Parameter()]
+  [switch]$Up,
+  [Parameter()]
+  [switch]$Down,
+  [Parameter()]
+  [switch]$Migrate
+)
+
+$session = New-PSSession -HostName home.arpa -UserName jose-guilherme
+
+function MigrateDevDb {
+  Invoke-Command -ScriptBlock { 
+    dotnet ef database update --project App
+  }
+}
+
+function MigrateTestDb {
+  $TestsEnvProps = Get-Content -Path ".\App\appsettings.Tests.json" -Raw | ConvertFrom-Json
+  $DbHost = $TestsEnvProps.DB_HOST
+  $DbUsername = $TestsEnvProps.DB_USER
+  $DbPassword = $TestsEnvProps.DB_PASSWORD
+  $DbDatabase = $TestsEnvProps.DB_DATABASE
+
+  Invoke-Command -ScriptBlock { param($dbhost, $user, $pass, $db)
+    dotnet ef database update `
+      --project App `
+      --connection "Host=$dbhost;Username=$user;Password=$pass;Database=$db"
+  } `
+    -ArgumentList $DbHost, $DbUsername, $DbPassword, $DbDatabase
+}
+
+if ($Up) {
+  Invoke-Command `
+    -Session $session `
+    -ScriptBlock {
+    Set-Location ~/repo/services/nextcondo; docker compose -f docker-compose-dev.yaml up -d
+  }
+}
+elseif ($Down) {
+  Invoke-Command `
+    -Session $session `
+    -ScriptBlock {
+    Set-Location ~/repo/services/nextcondo; docker compose -f docker-compose-dev.yaml down
+  }
+}
+
+if ($Migrate -and !$Down) {
+  Write-Host "Applying migrations to develoment database..."
+  MigrateDevDb
+  Write-Host "Applying migrations to test database..."
+  MigrateTestDb
+}
+
