716cfaf12229eb66a2aacefa9947b7c21de31589
Jose Guilherme <gui.kcoal+github@hotmail.com>
2024-08-15 23:24:52 -0300

    Update test script

--- run-tests.bash
+++ run-tests.bash
@@ -1,7 +1,13 @@
 #!bin/bash
 
+VolumeName="nextcondo-test-temp"
+
+docker volume create $VolumeName
+
 docker compose -f docker-compose-tests.yaml up \
   --abort-on-container-exit \
   --exit-code-from testapi
 
 docker compose -f docker-compose-tests.yaml down
+
+docker volume rm $VolumeName
